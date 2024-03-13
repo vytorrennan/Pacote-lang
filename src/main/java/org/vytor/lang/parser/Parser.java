d387f449cbfd8f8606814942f7e702a243a72224
vytorrennan <crosdeadbr@gmail.com>
2024-03-13 12:49:34 +0000

    Colocando tokenNavigation como variavel final no parser

--- src/main/java/org/vytor/lang/parser/Parser.java
+++ src/main/java/org/vytor/lang/parser/Parser.java
@@ -7,13 +7,11 @@ import java.util.ArrayList;
 public class Parser {
 
     private final ArrayList<Token> tokens;
-    private TokenNavigation tokenNavigation;
+    private final TokenNavigation tokenNavigation;
 
     public Parser(ArrayList<Token> tokens, TokenNavigation tokenNavigation) {
         this.tokens = tokens;
         this.tokenNavigation = tokenNavigation;
     }
-
-    
 
 }
